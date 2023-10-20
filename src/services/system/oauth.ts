b7170e2563300be97ef9c448f12c1ef6e1db9011
coding-hui <wecoding@yeah.net>
2023-10-21 00:34:12 +0800

    fix: oauth callback url

--- src/services/system/oauth.ts
+++ src/services/system/oauth.ts
@@ -1,4 +1,4 @@
-export const OAuthCallbackAPI = '/login/oauth/callback/';
+export const OAuthCallbackAPI = '/api/v1/oauth/callback/';
 
 export const getRedirectURL = (name?: string) => {
   return `${window.location.protocol}//${window.location.host}${OAuthCallbackAPI}${name}`;
